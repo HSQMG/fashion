c195c5d3185f32c38f6676b3b3c6d5446f570724
HSQMG <9981hoang@gmail.com>
2025-08-04 17:57:02 +0700

    updatea

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
 
+// const nextConfig: NextConfig = {
+
+//   images: {
+
+//     domains: ["www.personsoul.com"],
+//   },
+// };
 const nextConfig: NextConfig = {
+  output: "export", // Bật chế độ static export
+  // Bạn có thể thêm các cấu hình khác ở đây nếu cần
   images: {
     domains: ["www.personsoul.com"],
   },
 };
-
 export default nextConfig;
